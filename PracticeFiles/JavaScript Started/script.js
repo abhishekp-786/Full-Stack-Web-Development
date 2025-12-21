f0f5e62bf9ea7f0a228da2f9ee40da01a6f3592e
Abhishek Kumar Prajapati <prajapatiabhishekkumar15@gmail.com>
2025-12-21 09:01:34 +0530

    Pushed All the folder on github

---
+++ PracticeFiles/JavaScript Started/script.js
@@ -0,0 +1,27 @@
+
+// let num = Math.floor(Math.random()*6)+1;
+// console.log(num)
+
+// const carobj = {
+//     car1 : {
+//         name : 'Mercedes',
+//         model : 2025,
+//         color : "white"
+//     },
+//     car2 : {
+//         name: 'Baleno',
+//         model : 2024,
+//         color: "Grey"
+//     },
+//     car3 : {
+//         name : 'BMW',
+//         model : 2025,
+//         color: "Black"
+//     }
+// };
+
+const Person = {
+    name : "Abhishek",
+    age : 22,
+    city : "Phagwara"
+};
